e90d2380826554a8e958ca1463aa128ca0c47055
Anderson Gonçalves <anderson.coder@gmail.com>
2022-02-02 16:39:47 -0300

    Adicionando estrutura basica.

---
+++ index.js
@@ -0,0 +1,17 @@
+const receita = require('modules/receita');
+
+// Funcao princial
+async function main()
+{
+    // Codigo aqui
+    const cpf = '123.456.789-00';
+
+    // Calculo de Validade do cpf aqui
+
+
+
+    // Validação na receita aqui
+}
+
+
+main();
